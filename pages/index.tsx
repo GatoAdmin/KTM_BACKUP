84d74571cdae1e87ccb141db794523dfa098838f
Yunsup Sim <pedogunu@gmail.com>
2020-07-30 00:09:38 +0900

    chore: Add eslint setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import * as React from "react";
+import * as React from 'react';
 
-const App = () => {
-  return (
-    <>
-      <header>
-        <h2>To Do List</h2>
-      </header>
-      <main>Hello World</main>
-    </>
-  );
+const App: React.FC = () => {
+	return (
+		<>
+			<header>
+				<h2>To Do List</h2>
+			</header>
+			<main>Hello World</main>
+		</>
+	);
 };
 
 export default App;

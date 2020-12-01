a512bbf1d8486b431eb95b0ea3350bce593e25fa
Yunsup Sim <pedogunu@gmail.com>
2020-12-02 02:47:17 +0900

    feat: Add temporary translate hooks

---
+++ src/util/hooks/useTranslate.ts
@@ -0,0 +1,22 @@
+import {useState} from "react";
+
+interface I18nResource {
+  [key: string]: string
+}
+
+const supportedLang = ["ko", "vn"];
+export type SupportedLang = typeof supportedLang[number];
+
+type I18nStaticResources =  {
+  [key in SupportedLang]: I18nResource;
+}
+function useTranslate(
+  resource: I18nStaticResources
+): {t: (s: string) => string, lang: SupportedLang, changeLang: (s: string) => void} {
+  const [lang, setLang] = useState<SupportedLang>("ko");
+  const t = (searchString: string) => resource[lang][searchString];
+
+  return {t, lang, changeLang: (s: string) => setLang(s)}
+}
+
+export default useTranslate;

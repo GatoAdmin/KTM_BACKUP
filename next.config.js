e43baa081c6a606265974c758c686d0f972bf423
jonghopark95 <jong951005@gmail.com>
2021-02-20 18:32:54 +0900

    fix : add proxy settings because of cookie policy
    쿠키 Samesite = Lax 정책을 우회하기 위해 next.config.js 에 proxy 설정을 해두었습니다.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,32 @@
 module.exports = {
-	poweredByHeader: false,
-	trailingSlash: false,
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/,
-			issuer: {
-				test: /\.(ts|tsx)x?$/,
-			},
-			use: ['@svgr/webpack'],
-		});
+  poweredByHeader: false,
+  trailingSlash: false,
+  env: {
+    API_PATH: process.env.API_PATH,
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(ts|tsx)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
 
-		return config;
-	},
-	// enable when change to serverless-next with next.js 10
-	// i18n: {
-	// 	locales: ["ko", "vn"],
-	// 	defaultLocale: "ko",
-	// }
+    return config;
+  },
+  // 쿠키 정책 (SameSite)때문에 proxy 설정을 해주어야 합니다.
+  async rewrites() {
+    return [
+      {
+        source: '/api/:slug*',
+        destination: `${process.env.API_PATH}/api/:slug*/`,
+      },
+    ];
+  },
+  // enable when change to serverless-next with next.js 10
+  // i18n: {
+  // 	locales: ["ko", "vn"],
+  // 	defaultLocale: "ko",
+  // }
 };

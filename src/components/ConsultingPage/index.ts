a21d9211e727a2c3f6b1413e6f460284a1526760
Do-ho <hong7511059@koreatech.ac.kr>
2021-02-27 23:48:03 +0900

    feat: Add index file of consulting page

---
+++ src/components/ConsultingPage/index.ts
@@ -0,0 +1,15 @@
+import IntroductionSection from './IntroductionSection/IntroductionSection';
+import QnA from './QnA/QnA';
+import SubTitle from './SubTitle/Subtitle';
+import Tab from './Tab/Tab';
+import TitleBar from './TitleBar/TitleBar';
+import Accordion from './Accordion/Accordion';
+
+export {
+  IntroductionSection,
+  QnA,
+  SubTitle,
+  Tab,
+  TitleBar,
+  Accordion,
+};

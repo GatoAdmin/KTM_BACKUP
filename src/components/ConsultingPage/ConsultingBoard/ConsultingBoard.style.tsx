0bced68518cbfab4056a1fd40473cc87bb2a2bcb
Do-ho <hong7511059@koreatech.ac.kr>
2021-02-28 02:36:12 +0900

    feat: add pagination
    - 게시판의 이동을 감지해주는 pagination 구현

--- src/components/ConsultingPage/ConsultingBoard/ConsultingBoard.style.tsx
+++ src/components/ConsultingPage/ConsultingBoard/ConsultingBoard.style.tsx
@@ -1,8 +1,14 @@
-import styled, { css } from 'styled-components';
-import { fontColor, mainColor } from '@util/style/color';
+import styled from 'styled-components';
+import { mainColor } from '@util/style/color';
+import LeftArrowSVG from '@assets/svg/arrow_left_icon.svg';
+import RightArrowSVG from '@assets/svg/arrow_right_icon.svg';
 
 interface BoardThProps {
   width: string;
+}
+
+interface ArrowProps {
+  disable: boolean;
 }
 
 export const ConsultingBoardContainer = styled.section`
@@ -12,6 +18,7 @@ export const ConsultingBoardContainer = styled.section`
   width: 1000px;
   padding: 20px;
   user-select: none;
+  margin-bottom: 50px;
 `;
 
 export const SubTitleWrap = styled.div`
@@ -38,18 +45,83 @@ export const WriteButton = styled.button`
 export const BoardTable = styled.table`
   table-layout: fixed;
   width: 100%;
+  border-collapse: collapse;
 `;
 
-export const BoardTr = styled.tr``;
+export const BoardTr = styled.tr`
+  cursor: pointer;
+  &:hover {
+    background-color: ${mainColor};
+    color: white;
+  }
+`;
 
 export const BoardTh = styled.th<BoardThProps>`
-  padding: 18px 10px 5px 10px;
+  padding: 18px 10px;
   width: ${(props) => props.width};
   text-align: left;
   font-size: 16px;
+  border-bottom: 2px solid #C4C4C4;
+`;
+
+export const BoardEmptyTr = styled.tr`
+  height: 50px;
+`;
+
+export const BoardEmptyMessage = styled.td`
+  text-align: center;
+  color: #9E9E9E;
+  border-bottom: 2px solid #C4C4C4;
+  font-weight: bold;
 `;
 
 export const BoardTd = styled.td`
+  padding-left: 10px;
+  text-align: left;
+  height: 50px;
+  border-bottom: 1px solid #C4C4C4;
+`;
+
+export const TitleWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const CompleteLabel = styled.div`
+  background: ${mainColor};
+  border-radius: 100px;
+  color: white;
+  padding: 5px 10px;
   text-align: center;
-  color: #9E9E9E;
+  font-size: 12px;
+  margin-left: 10px;
+`;
+
+export const PaginationContainer = styled.div`
+  display:flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const PageText = styled.h5`
+  margin-right: 33px;
+`;
+
+export const LeftArrow = styled(LeftArrowSVG)<ArrowProps>`
+  height: 12px;
+  path {
+    stroke: ${(props) => (props.disable ? 'lightgrey' : 'black')};
+    stroke-width: 3px;
+  }
+  margin-right: 30px;
+  ${(props) => (props.disable ? null : 'cursor: pointer')};
+`;
+
+export const RightArrow = styled(RightArrowSVG)<ArrowProps>`
+  height: 12px;
+  path {
+    stroke: ${(props) => (props.disable ? 'lightgrey' : 'black')};
+    stroke-width: 3px;
+  }
+  ${(props) => (props.disable ? null : 'cursor: pointer')};
 `;
--- src/components/ConsultingPage/ConsultingBoard/ConsultingBoard.tsx
+++ src/components/ConsultingPage/ConsultingBoard/ConsultingBoard.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable camelcase */
 import React, { useState } from 'react';
 import {
-  SubTitle, TitleBar,
+  SubTitle,
 } from '@components/ConsultingPage';
+import mockData from '@assets/i18n/myConsultData.json';
 import {
   ConsultingBoardContainer,
   SubTitleWrap,
@@ -10,6 +12,14 @@ import {
   BoardTh,
   BoardTr,
   BoardTd,
+  BoardEmptyMessage,
+  CompleteLabel,
+  TitleWrap,
+  BoardEmptyTr,
+  PageText,
+  PaginationContainer,
+  LeftArrow,
+  RightArrow,
 } from './ConsultingBoard.style';
 
 interface ConsultingBoardProps {
@@ -18,8 +28,63 @@ interface ConsultingBoardProps {
   changeLang: (s: string) => void;
 }
 
+interface userConsultDataType {
+  id: number;
+  user_id: number;
+  type: string;
+  title: string;
+  contents: string;
+  is_answer: boolean;
+  uploaded_at: string;
+  answer: string
+}
+
+interface pagesType {
+  current_page: number;
+  per_page: number;
+  max_page: number;
+}
+
+interface boardTableDatatype {
+  pages: pagesType;
+  user_qna: Array<userConsultDataType>;
+}
+
 const ConsultingBoard: React.FC<ConsultingBoardProps> = ({ t, lang, changeLang }) => {
   const [offset, setOffset] = useState(1);
+  const convertId = (id: number) => String(id).padStart(5, '0');
+  const bEmptyData = mockData.user_qna.length === 0;
+  const bDisableLeftArrow = offset <= 1;
+  const bDisableRightArrow = offset >= mockData.pages.max_page;
+
+  const handlingLeftArrowClick = () => {
+    if (bDisableLeftArrow) return;
+    setOffset(offset - 1);
+  };
+
+  const handlingRightArrowClick = () => {
+    if (bDisableRightArrow) return;
+    setOffset(offset + 1);
+  };
+
+  const userConsultTable = mockData.user_qna.map(({
+    id, title, is_answer, uploaded_at,
+  }) => (
+    <BoardTr>
+      <BoardTd>
+        { convertId(id) }
+      </BoardTd>
+      <BoardTd>
+        <TitleWrap>
+          { title }
+          { is_answer ? <CompleteLabel> 답변완료 </CompleteLabel> : null}
+        </TitleWrap>
+      </BoardTd>
+      <BoardTd>
+        { uploaded_at }
+      </BoardTd>
+    </BoardTr>
+  ));
 
   return (
     <ConsultingBoardContainer>
@@ -28,18 +93,27 @@ const ConsultingBoard: React.FC<ConsultingBoardProps> = ({ t, lang, changeLang }
         <WriteButton> 글쓰기 </WriteButton>
       </SubTitleWrap>
       <BoardTable>
-        <BoardTr>
-          <BoardTh width="10%"> 번호 </BoardTh>
-          <BoardTh width="70%"> 제목 </BoardTh>
-          <BoardTh width="20%"> 등록일 </BoardTh>
-        </BoardTr>
-        <TitleBar />
-        <BoardTr>
-          <BoardTd colSpan="3"> 나의 1:1 상담 내역이 없습니다. </BoardTd>
-        </BoardTr>
-
-        <TitleBar />
+        <BoardTh width="10%"> 번호 </BoardTh>
+        <BoardTh width="78%"> 제목 </BoardTh>
+        <BoardTh width="12%"> 등록일 </BoardTh>
+        {bEmptyData
+          ? (
+            <BoardEmptyTr>
+              <BoardEmptyMessage colSpan="3"> 나의 1:1 상담 내역이 없습니다. </BoardEmptyMessage>
+            </BoardEmptyTr>
+          ) : userConsultTable}
       </BoardTable>
+      {bEmptyData ? (
+        null
+      ) : (
+        <PaginationContainer>
+          <PageText>
+            {`${offset} / ${mockData.pages.max_page}`}
+          </PageText>
+          <LeftArrow disable={bDisableLeftArrow} onClick={handlingLeftArrowClick} />
+          <RightArrow disable={bDisableRightArrow} onClick={handlingRightArrowClick} />
+        </PaginationContainer>
+      )}
     </ConsultingBoardContainer>
   );
 };

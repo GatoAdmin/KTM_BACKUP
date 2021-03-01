c6c0c50e1730f2f6f080350fa0bc46e5d2658ac1
Do-ho <hong7511059@koreatech.ac.kr>
2021-03-01 23:55:43 +0900

    feat: route consult write page

---
+++ src/views/ConsultingPage/WritePage/WritePage.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '@components/Shared/Header/Header';
+import Footer from '@components/Shared/Footer/Footer';
+import { NextPage } from 'next';
+import useTranslate from '@util/hooks/useTranslate';
+import { IntroductionSection } from '@components/ConsultingPage';
+import i18nResource from '@assets/i18n/landingPage.json';
+import {
+  FontProvider, Introduction,
+} from './WritePage.style';
+
+const WritePage: NextPage = () => {
+  const { t, lang, changeLang } = useTranslate(i18nResource);
+  return (
+    <FontProvider lang={lang}>
+      <Header
+        t={t}
+        lang={lang}
+        changeLang={changeLang}
+      />
+      <Introduction>
+        <IntroductionSection t={t} />
+      </Introduction>
+
+      <Footer />
+    </FontProvider>
+  );
+};
+
+export default WritePage;

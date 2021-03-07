8b3046c899c667c5e331aac97ab43a3efc483af4
Do-ho <hong7511059@koreatech.ac.kr>
2021-03-07 17:52:08 +0900

    feat: add inactive button style
    - button props에 active를 줌으로 input에 대한 버튼의 스타일을 정의해줌

--- src/components/Shared/Button/Button.tsx
+++ src/components/Shared/Button/Button.tsx
@@ -1,27 +1,29 @@
 import { mainColor } from '@util/style/color';
-import React from 'react';
+import React, { Dispatch } from 'react';
 import styled from 'styled-components';
 
 interface ButtonProps {
   onClick: () => void;
+  active?: boolean;
 }
 
-const StyledButton = styled.button`
+const StyledButton = styled.button<ButtonProps>`
   padding: 8px 20px;
-  background: ${mainColor};
+  background: ${(props) => (props.active ? mainColor : 'rgba(255, 114, 99, 0.08);')};
   border: 1px solid ${mainColor};
   box-sizing: border-box;
   border-radius: 100px;
-  color: white;
+  color: ${(props) => (props.active ? 'white' : 'rgba(223, 77, 61, 1)')};
   font-weight: bold;
   font-size: 14px;
   line-height: 19px;
   outline: none;
   cursor: pointer;
+  width: fit-content;
 `;
 
-const Button: React.FC<ButtonProps> = ({ onClick, children }) => (
-  <StyledButton onClick={onClick}>
+const Button: React.FC<ButtonProps> = ({ onClick, children, active = true }) => (
+  <StyledButton onClick={onClick} active={active}>
     {children}
   </StyledButton>
 );

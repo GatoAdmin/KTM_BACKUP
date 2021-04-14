3a9bea7674be341d3c38c7a1af2cf01faae764df
Do-ho <hong7511059@koreatech.ac.kr>
2021-04-15 00:37:22 +0900

    feat: add mypage component index.ts file

---
+++ src/components/MyPage/index.ts
@@ -0,0 +1,15 @@
+import SelectTab from './SelectTab/SelectTab';
+import MyUniversityList from './MyUniversityList/MyUniversityList';
+import MyInfomation from './MyInfomation/MyInfomation';
+import MySolutionSection from './MySolutionSection/MySolutionSection';
+import RefundSection from './RefundSection/RefundSection';
+import MainArea from './MainArea/MainArea';
+
+export {
+  SelectTab,
+  MyUniversityList,
+  MyInfomation,
+  MySolutionSection,
+  RefundSection,
+  MainArea,
+};

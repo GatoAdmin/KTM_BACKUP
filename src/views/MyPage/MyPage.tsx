fb39b16d56065ede283c689c27b362d0bc9a3bf2
Do-ho <hong7511059@koreatech.ac.kr>
2021-04-15 00:37:44 +0900

    feat: add Mypage view

--- src/views/MyPage/MyPage.tsx
+++ src/views/MyPage/MyPage.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import Header from '@components/Shared/Header/Header';
+import Footer from '@components/Shared/Footer/Footer';
 import { NextPage } from 'next';
+import useTranslate from '@util/hooks/useTranslate';
+import i18nResource from '@assets/i18n/myPage.json';
+import {
+  MainArea,
+} from '@components/MyPage';
+import MainPageContainer from './MyPage.style';
 
-const MyPage: NextPage = () => <div>testing</div>;
+const MyPage: NextPage = () => {
+  const { t, lang, changeLang } = useTranslate(i18nResource);
+
+  return (
+    <MainPageContainer lang={lang}>
+      <Header
+        background="dark"
+        t={t}
+        changeLang={changeLang}
+        lang={lang}
+      />
+      <MainArea t={t} />
+      <Footer />
+    </MainPageContainer>
+  );
+};
 
 export default MyPage;

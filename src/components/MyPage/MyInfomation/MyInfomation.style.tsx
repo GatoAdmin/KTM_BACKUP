99ade1452c7e3b3634bfd58f339f044dd54e628d
Do-ho <hong7511059@koreatech.ac.kr>
2021-04-18 12:18:25 +0900

    feat: set MyInfomationContainer and usePromise

--- src/components/MyPage/MyInfomation/MyInfomation.style.tsx
+++ src/components/MyPage/MyInfomation/MyInfomation.style.tsx
@@ -1,2 +1,6 @@
 import { fontColor, mainColor } from '@util/style/color';
 import styled from 'styled-components';
+
+export const MyInfomationContainer = styled.div`
+  padding: 50px;
+`;
--- src/components/MyPage/MyInfomation/MyInfomation.tsx
+++ src/components/MyPage/MyInfomation/MyInfomation.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
-import { } from './MyInfomation.style';
+import { Title } from '@components/MyPage';
+import API from '@util/api';
+import usePromise from '@util/hooks/usePromise';
+import {
+  MyInfomationContainer,
+} from './MyInfomation.style';
 
 interface MyInfomationProps {
   t: (s:string) => string;
 }
 
-const MyInfomation: React.FC<MyInfomationProps> = () => (
-  <>
+const MyInfomation: React.FC<MyInfomationProps> = () => {
+  const getMyService = async () => {
+    const service = await API.getMyService();
+    return service;
+  };
 
-  </>
-);
+  const [loading, resolved, error] = usePromise(getMyService, []);
+
+  if (loading) return <> </>; // 나중에 스피너나 빈프레임 넣으면 좋을 것 같습니다 ㅎㅎ
+  if (error) window.location.href = '/';
+  if (!resolved) return null;
+
+  return (
+    <MyInfomationContainer>
+      <Title> 개인정보 변경 </Title>
+    </MyInfomationContainer>
+  );
+};
 
 export default MyInfomation;

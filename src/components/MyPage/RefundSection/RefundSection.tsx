1511bbed4ee86f19c8b614db726ff84a8e75ac2a
Do-ho <hong7511059@koreatech.ac.kr>
2021-04-20 15:43:53 +0900

    feat: add basic code

--- src/components/MyPage/RefundSection/RefundSection.tsx
+++ src/components/MyPage/RefundSection/RefundSection.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { } from './RefundSection.style';
+import API from '@util/api';
+import usePromise from '@util/hooks/usePromise';
 
 interface RefundSectionProps {
-  t: () => void
+  t: (s:string) => string;
+  payId: number;
 }
 
-const RefundSection: React.FC<RefundSectionProps> = () => (
-  <>
+const RefundSection: React.FC<RefundSectionProps> = ({ payId }) => {
+  const getMyRefundInfo = async () => {
+    const refund = await API.getMyRefundInfo(payId);
+    return refund;
+  };
 
-  </>
-);
+  const [loading, resolved, error] = usePromise(getMyRefundInfo, []);
+
+  console.log(resolved);
+
+  return (
+    <>
+
+    </>
+  );
+};
 
 export default RefundSection;

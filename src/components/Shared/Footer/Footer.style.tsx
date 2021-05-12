26ae61b4a3feabd83348a175c7de0ff786397126
jonghopark95 <jong951005@gmail.com>
2021-05-12 17:02:20 +0900

    fix: Fix Footer

--- src/components/Shared/Footer/Footer.style.tsx
+++ src/components/Shared/Footer/Footer.style.tsx
@@ -1,45 +1,68 @@
 import styled from 'styled-components';
-import { borderColor, mainColor } from '@util/style/color';
-import LogoIcon from '../../../assets/logo.svg';
+import { borderColor } from '@util/style/color';
 
 export const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: center;
   width: 100%;
-  height: 200px;
+  height: 180px;
   background-color: ${borderColor};
 `;
 
 export const FooterContent = styled.div`
   display: flex;
-  width: 1400px;
-  margin: 0 auto;
-  padding: 50px 0;
+  flex-direction: column;
+  width: 1018px;
+  padding: 26px 0;
 `;
 
-export const LogoContainer = styled.div`
+export const MainInfoWrapper = styled.div`
   display: flex;
-  height: 100px;
-  margin-right: 100px;
-  color: ${mainColor};
-  font: 900 45px/100px Nunito, sans-serif;
+  align-items: center;
+  height: 24px;
+
+  margin-bottom: 25px;
 `;
 
-export const Logo = styled(LogoIcon)`
-  width: 100px;
-  height: 100px;
-  margin-right: 20px;
+export const MainInfo = styled.span`
+  font-family: Noto Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 19px;
 
-  > path {
-    fill: ${mainColor};
-  }
+  color: #212121;
+
+  cursor: pointer;
 `;
 
-export const SponsorContainer = styled.div`
+export const MainInfoDelimiter = styled.span`
+  width: 1px;
+  height: 13px;
+  background-color: black;
+  margin: 10px;
+`;
+
+export const SubInfoWrapper = styled.div`
   display: flex;
-  opacity: 0.6;
+  align-items: center;
+  height: 24px;
 `;
 
-export const SponsorImage = styled.img`
-  width: 200px;
-  height: 100px;
-  object-fit: contain;
+export const SubInfo = styled(MainInfo)`
+  font-weight: normal;
+  cursor: default;
+`;
+
+export const SubInfoDelimiter = styled(MainInfoDelimiter)`
+  background-color: #212121;
+`;
+
+export const Copyright = styled.span`
+  margin-top: 15px;
+  font-family: Noto Sans;
+  font-size: 12px;
+  line-height: 16px;
+
+  color: #212121;
 `;
--- src/components/Shared/Footer/Footer.tsx
+++ src/components/Shared/Footer/Footer.tsx
@@ -3,22 +3,70 @@ import { LogoContainer } from '@components/LandingPage/Header/Header.style';
 import useTranslate from '@util/hooks/useTranslate';
 import i18nResource from '../../../assets/i18n/landingPage.json';
 import {
+  Copyright,
   FooterContainer,
-  FooterContent, //
+  FooterContent,
+  MainInfo,
+  MainInfoDelimiter,
+  MainInfoWrapper, //
   SponsorContainer,
   SponsorImage,
+  SubInfo,
+  SubInfoDelimiter,
+  SubInfoWrapper,
 } from './Footer.style';
 
+const COPYRIGHT_LABEL = 'COPYRIGHT(C)  KSTUMM.  ALL RIGHTS RESERVED';
+
 const Footer: React.FC = ({ t, lang }) => {
+  const MainInfoArr = [
+    { label: t('footer-main-label-1'), path: '/' },
+    { label: t('footer-main-label-2'), path: '/' },
+    { label: t('footer-main-label-3'), path: '/' },
+    { label: t('footer-main-label-4'), path: '/' },
+  ];
+
+  const SubInfoArr = [
+    { pre: t('footer-company-name'), val: t('footer-company-name-val') },
+    { pre: t('footer-ceo'), val: t('footer-ceo-val') },
+    { pre: t('footer-phone-no'), val: t('footer-phone-no-val') },
+    { pre: t('footer-fax'), val: t('footer-fax-val') },
+    { pre: t('footer-address'), val: t('footer-address-val') },
+    { pre: t('footer-company-no'), val: t('footer-company-no-val') },
+    { pre: t('footer-selling-no'), val: t('footer-selling-no-val') },
+  ];
+
   return (
     <FooterContainer>
       <FooterContent>
-        <LogoContainer>{t('university')}</LogoContainer>
-        <SponsorContainer>
-          <SponsorImage src="/images/CN_support.png" alt="충남컨텐츠코리아랩" />
-        </SponsorContainer>
+        <MainInfoWrapper>
+          {MainInfoArr.map(({ label, path }, idx) => (
+            <>
+              <MainInfo key={label}>{`${label}`}</MainInfo>
+              {idx !== 3 && <MainInfoDelimiter />}
+            </>
+          ))}
+        </MainInfoWrapper>
+        <SubInfoWrapper>
+          {SubInfoArr.slice(0, 4).map(({ pre, val }) => (
+            <>
+              <SubInfo key={val}>{pre === '' ? `${val}` : `${pre} : ${val}`}</SubInfo>
+              <SubInfoDelimiter />
+            </>
+          ))}
+        </SubInfoWrapper>
+        <SubInfoWrapper>
+          {SubInfoArr.slice(4).map(({ pre, val }) => (
+            <>
+              <SubInfo key={val}>{pre === '' ? `${val}` : `${pre} : ${val}`}</SubInfo>
+              <SubInfoDelimiter />
+            </>
+          ))}
+        </SubInfoWrapper>
+        <Copyright>{COPYRIGHT_LABEL}</Copyright>
       </FooterContent>
     </FooterContainer>
   );
 };
+
 export default Footer;
